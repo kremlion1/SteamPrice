d99b9849ccf98f4c3c3cf9ce27c1059bbfe08e5e
scoopee <kvaziomich@gmail.com>
2015-09-13 23:05:36 +0500

    just testing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button7 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.downloadButton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button3 = new System.Windows.Forms.Button();
@@ -67,13 +67,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Загрузить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.downloadButton.Location = new System.Drawing.Point(13, 74);
+            this.downloadButton.Name = "button1";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 8;
+            this.downloadButton.Text = "Загрузить";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
@@ -278,7 +278,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.downloadButton);
             this.Controls.Add(this.button7);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -292,7 +292,7 @@
         #endregion
 
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button downloadButton;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button3;
